a1642354e2305e4c8f26394b82a01fb8ae18d413
shantha26 <shanthalakshmi2606@gmail.com>
2024-09-24 12:44:20 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,8 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    if (name === '' || email === '') {
+        alert('Please fill in all fields');
+        event.preventDefault();
+    }
+});
