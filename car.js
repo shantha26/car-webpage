f4576225657f4b26747e68b704fbcba8378b707d
shantha26 <shanthalakshmi2606@gmail.com>
2024-10-04 08:00:13 +0530

    Update car.js

--- car.js
+++ car.js
@@ -1,8 +1,8 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    if (name === '' || email === '') {
-        alert('Please fill in all fields');
+    if (name && email) {
+        alert('Thnk you for your message');
         event.preventDefault();
     }
 });
